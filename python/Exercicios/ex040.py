f6cd76ff3df4502beeb309677b7ba836316e3fd6
Robert Marchinhaki <89221879+Robert-Marchinhaki@users.noreply.github.com>
2021-10-06 21:54:56 -0300

    MEDIA TRIMESTRAL

--- python/Exercicios/ex040.py
+++ python/Exercicios/ex040.py
@@ -17,7 +17,6 @@ else:
 nota2 = float(input('Segunda nota: '))
 media = (nota1 + nota2) / 2
 print(f'Tirando {nota1} e {nota2}, a média do aluno é {media}')
-
 if 7 > media >= 5:
     print('O aluno está em RECUPERAÇÃO!')
 elif media < 5:
