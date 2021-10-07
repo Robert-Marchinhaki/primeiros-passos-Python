4f0aff301d7109959901db773768e90b8c67932f
Robert Marchinhaki <89221879+Robert-Marchinhaki@users.noreply.github.com>
2021-10-06 21:22:44 -0300

    Contagem Regressiva

---
+++ python/Exercicios/ex046.py
@@ -0,0 +1,29 @@
+# CONTAGEM REGRESSIVA
+
+from time import sleep
+print('Contagem regressiva')
+for c in range(10, -1, -1):
+    sleep(1)
+    print(c)
+sleep(1)
+print('Chegamos ao final da contagem.')
+
+
+# for c in range(1,10):
+#     print(c)
+# print('FIM!')
+
+
+# from time import sleep
+# for c in range(1, 101, 2):
+#     sleep(0.5)
+#     print(c)
+# sleep(5)
+# print('Terminas sua contagem de 2 em 2, contando em 0,5sec.')
+
+
+# number = int(input('Enter a number: '))
+# for timetables in range(1, 11):
+#     print(f'{number} x {timetables} = {number * timetables}')
+
+
