799f3eea482cfbabda758e0038a90f999be0636c
Robert Marchinhaki <89221879+Robert-Marchinhaki@users.noreply.github.com>
2021-10-06 21:55:33 -0300

    V 2.0 TRIÂNGULOS

--- python/Exercicios/ex042.py
+++ python/Exercicios/ex042.py
@@ -26,6 +26,7 @@ else:
 r1 = float(input('Digite o valor da primeira reta: '))
 r2 = float(input('Digite o valor da segunda reta: '))
 r3 = float(input('Digite o valor da terceira reta: '))
+
 if r1 < r2 + r3 and r2 < r1 + r3 and r3 < r1 + r2:
     print('Os segmentos podem formar um triângulo do tipo', end='')
     if r1 == r2 == r3:
