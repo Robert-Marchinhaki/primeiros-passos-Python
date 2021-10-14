14e81d5a7280e8e6ed09cca0fd1de33fb7fad92e
Robert Marchinhaki <89221879+Robert-Marchinhaki@users.noreply.github.com>
2021-10-14 17:58:42 -0300

    AULA 16

---
+++ python/AULAS/aula016.py
@@ -0,0 +1,28 @@
+# lanche = ('Hambúrguer', 'Suco', 'Pizza', 'Pudim')
+
+# for comida in lanche:
+#     print(lanche)
+
+# for cont in range(0, len(lanche)):
+#     print(f'{cont + 1}° eu vou comer {lanche[cont]}')
+
+# for pos, cont in enumerate(lanche):
+#     print(f'{pos + 1}° eu vou comer {cont}')
+
+# print(sorted(lanche))
+# print(lanche)
+
+# a = (2, 5, 4)
+# b = (5, 8, 1, 2)
+# c = a + b
+# d = b + a
+# print(a, end='a ')
+# print(b, end='b ')
+# print(c, end='c ')
+# print(d, end='d ')
+
+# pessoa = ('Gustavo', 39, 'M', 99.88)
+# del(pessoa)
+# print(pessoa)
+
+
