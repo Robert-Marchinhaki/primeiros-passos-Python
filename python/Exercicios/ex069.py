483721fcae1c7bade58b2966b575277c037222aa
Robert Marchinhaki <89221879+Robert-Marchinhaki@users.noreply.github.com>
2021-10-14 17:59:17 -0300

    PROGRAMA PARA LER IDADE E SEXO DE VÁRIAS PESSOAS

--- python/Exercicios/ex069.py
+++ python/Exercicios/ex069.py
@@ -1,32 +1,30 @@
 # PROGRAMA PARA LER IDADE E SEXO DE VÁRIAS PESSOAS
 
-homem = 0
-mulher = 0
-contador = 0
+homem = mulher = contador = 0
 while True:
     idade = int(input('Idade: '))
     sexo = str(input('Sexo [M/F]: ')).upper().strip()[0]
-    while sexo not in 'MmFf':
+    while sexo not in 'MF':
         sexo = str(input('Não entendi. Digite novamente [M/F]: ')).upper().strip()[0]
     cadastro = str(input('Quer continuar [S/N]? ')).upper().strip()[0]
-    while cadastro not in 'SsNn':
+    while cadastro not in 'SN':
         cadastro = str(input('Não entendi. Digite novamente: [S/N] ')).upper().strip()[0]
     # QUANTAS PESSOAS TEM MAIS DE 18 ANOS
-    if cadastro in 'Ss':
+    if cadastro in 'S':
         if idade > 18:
             contador += 1
         # QUANTOS 'HOMENS' FORAM CADASTRADOS
-        if sexo in 'Mm':
+        if sexo in 'M':
             homem += 1
         # QUANTAS 'MULHERES' TEM MENOS DE 20 ANOS
-        if sexo in 'Ff' and idade < 20:
+        if sexo in 'F' and idade < 20:
             mulher += 1
     else:
         if idade > 18:
             contador += 1
-        if sexo in 'Mm':
+        if sexo in 'M':
             homem += 1
-        if sexo in 'Ff' and idade < 20:
+        if sexo in 'F' and idade < 20:
             mulher += 1
         break
 
