d60da01cb2d5ba9b9d2c4d6983a3c78fd7bee3bd
Robert Marchinhaki <89221879+Robert-Marchinhaki@users.noreply.github.com>
2021-10-30 11:30:57 -0300

    pritando itens em ordem

---
+++ python/Exercicios/ex076.py
@@ -0,0 +1,13 @@
+produtos = ('lápis de cor', 17.90, 'Caneta Permanente', 13.99, 'Estojo duplo', 27.50,
+            'Borracha (2uni)', 18.85, 'caderno desenho', 39.90, 'caderno 10 mat',
+            19.90, 'kit 4 caderno 1 mat', 34.90, 'cola', 6.90, 'bolsa', 64.90)
+
+print(f'{produtos[0]}.....................R$ {produtos[1]:.2f}'
+      f'\n{produtos[2]}................R$ {produtos[3]:.2f}'
+      f'\n{produtos[4]}.....................R$ {produtos[5]:.2f}'
+      f'\n{produtos[6]}..................R$ {produtos[7]:.2f}'
+      f'\n{produtos[8]}..................R$ {produtos[9]:.2f}'
+      f'\n{produtos[10]}...................R$ {produtos[11]:.2f}'
+      f'\n{produtos[12]}..............R$ {produtos[13]:.2f}'
+      f'\n{produtos[14]}.............................R$ {produtos[15]:.2f}'
+      f'\n{produtos[16]}............................R$ {produtos[17]:.2f}')
